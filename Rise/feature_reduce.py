e51770b7cde7dc830b43b66bf4d7126e3d9e2d28
Namida M <namidam@gmail.com>
2017-10-25 14:58:14 +0530

    Neural mainly. Also started knn and random forest, but not progress. Last part of neural can be referred for submission format

--- Rise/feature_reduce.py
+++ Rise/feature_reduce.py
@@ -7,16 +7,27 @@ from sklearn import linear_model
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 import sklearn.metrics as met
 
+def csv_writer(data,filename,mode):
+		with open(filename,mode) as fname:
+			np.savetxt(fname,data,delimiter=',',newline='\n')
 #print("BEFORE")
 train_data=preprocessing.scale(np.loadtxt('imputed_train_median.csv', delimiter=','))
-#test_data=preprocessing.scale(np.loadtxt('../DS3/test.csv', delimiter=','))
+test_data=preprocessing.scale(np.loadtxt('imputed_test_median.csv', delimiter=','))
 
-train_labels=np.loadtxt('train_labels.csv', delimiter=',')-1
-X_train, X_validate, y_train, y_validate = train_test_split(train_labels, y,stratify=y, test_size=0.25)
+train_labels=np.loadtxt('train_labels.csv', delimiter=',')
+X_train, X_validate, Y_train, Y_validate = train_test_split(train_data, train_labels,stratify=train_labels, test_size=0.25)
 #test_labels=np.loadtxt('../DS3/test_labels.csv', delimiter=',')-1
 
-lda =LinearDiscriminantAnalysis(n_components=500)
-chosen_feature=lda.fit_transform(X_train,y_train)
+lda =LinearDiscriminantAnalysis(n_components=2000)
 
-x_test_chosen=lda.transform(test_data)
+X_train=lda.fit_transform(X_train,Y_train)
+X_validate=lda.transform(X_validate)
+X_test=lda.transform(test_data)
+
+csv_writer(X_train,"X_train.csv","wb")
+csv_writer(X_test,"X_test.csv","wb")
+csv_writer(X_validate,"X_validate.csv","wb")
+csv_writer(Y_validate,"Y_validate.csv","wb")
+csv_writer(Y_train,"Y_train.csv","wb")
+
 
